4528fe1e949a9acecfc8f21af8b6a34d1a664876
KaylaK1 <44605348+KaylaK1@users.noreply.github.com>
2024-03-13 14:00:38 -0400

    mocking/testing overview done - need to refactor

--- crackingcoding/src/test/java/com/crackingcoding/BasicTests.java
+++ crackingcoding/src/test/java/com/crackingcoding/BasicTests.java
@@ -1,5 +1,36 @@
 package com.crackingcoding;
 
-public class BasicTests {
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 
+public class BasicTests {
+    @Test
+    @DisplayName("First number is greater than the second")
+    public void compare() {
+        Basics basicTests = new Basics();
+        int value = basicTests.compare(2, 1); // should return -1
+
+        Assertions.assertEquals(1, value);
+    }
+
+    @Test
+    @DisplayName("First number is less than the second")
+    public void compare2() {
+        Basics basicTests = new Basics();
+        int value = basicTests.compare(1, 2); // should return -1
+
+        Assertions.assertEquals(-1, value);
+    }
+
+    @Test
+    @DisplayName("Array Sorted")
+    public void sortArray() {
+        Basics basicTests = new Basics();
+        int[] array = { 5, 8, 3, 9, 1, 6 };
+        basicTests.sortArray(array);
+
+        Assertions.assertArrayEquals(new int[] { 1, 3, 5, 6, 8, 9 }, array);
+    }
 }
